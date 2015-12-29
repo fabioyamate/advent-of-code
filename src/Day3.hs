74577af18060c998a49b0d3754e5dc0933aadb2d
Fabio Yamate <fabioyamate@gmail.com>
2015-12-28 22:56:59 -0200

    Add day3 part2 solution

--- src/Day3.hs
+++ src/Day3.hs
@@ -3,6 +3,7 @@
 module Day3 where
 
 import qualified Data.Set as S
+import Data.List (foldl')
 
 type Position = (Int, Int)
 type Direction = Char
@@ -26,7 +27,24 @@ deliveries position path = scanr move position path
 countUnique :: (Eq a, Ord a) => [a] -> Int
 countUnique = S.size . S.fromList
 
+part1 :: [Direction] -> Int
+part1 = countUnique . deliveries initialPosition
+
+partitionBy2 [] = []
+partitionBy2 [x] = []
+partitionBy2 (x:y:xs) = (x,y) : (partitionBy2 xs)
+
+moveBoth (p1,p2,houses) (d1,d2) = (p1',p2',houses')
+    where (p1',p2') = (move d1 p1, move d2 p2)
+          houses' = S.insert p1' . S.insert p2' $ houses
+
+part2 :: [Direction] -> Int
+part2 moves = S.size houses
+    where splittedMoves = partitionBy2 moves
+          (_,_,houses) = foldl' moveBoth ((0,0),(0,0),S.singleton (0,0)) splittedMoves
+
 main :: IO ()
 main = do
-     directions <- getContents
-     print $ countUnique $ deliveries initialPosition directions
+    directions <- getContents
+    print $ part1 directions
+    print $ part2 directions

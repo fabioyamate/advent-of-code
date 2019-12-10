743e01bb58d1dddf68837dd3f30a439fe918f8e2
Fabio Yamate <fabioyamate@gmail.com>
2019-12-09 21:59:51 -0300

    Add advent-of-code 2019 day 6 part 2

--- clj/src/advent-of-code-2019/day06.clj
+++ clj/src/advent-of-code-2019/day06.clj
@@ -2,7 +2,8 @@
   (:use clojure.test)
   (:require
    [clojure.java.io :as io]
-   [clojure.zip :as zip]))
+   [clojure.zip :as zip]
+   [clojure.set :as set]))
 
 (defn prewalk
   [f loc]
@@ -56,7 +57,6 @@
        (build-tree {:n "COM"})
        compute-heights
        (tree-seq :n :c)
-       #_(map #(select-keys % [:n :h]))
        (filter :n)
        (map :h)
        (reduce +)))
@@ -67,3 +67,51 @@
 (with-open [rdr (io/reader (io/resource "advent-of-code-2019/day06.in"))]
   (solve-1 (line-seq rdr)))
 ;; => 110190
+
+(defn find-node
+  [node loc]
+  (loop [loc loc]
+    (if (= node (:n (zip/node loc)))
+      loc
+      (recur (zip/next loc)))))
+
+(defn abs [x] (if (neg? x) (- x) x))
+
+(defn solve-2
+  [edges node-1 node-2]
+  (let [zp (->> edges
+                (map parse-edge)
+                (build-tree {:n "COM"})
+                compute-heights
+                tree->zipper)
+        node-a (find-node node-1 zp)
+        height-a (:h (zip/node node-a))
+        node-a-path (path-nodes node-a)
+        node-b (find-node node-2 zp)
+        height-b (:h (zip/node node-b))
+        node-b-path (path-nodes node-b)
+        deepest-common-node-height (->> (set/intersection (set node-a-path)
+                                                          (set node-b-path))
+                                        (sort-by :h)
+                                        last
+                                        :h)]
+    (- (+ (abs (- height-a deepest-common-node-height))
+          (abs (- height-b deepest-common-node-height)))
+       2))) ;; the nodes itself doesn't count, it wanna orbit
+
+(defn path-nodes
+  [loc]
+  (->> (zip/path loc)
+       (map #(select-keys % [:n :h]))))
+
+(deftest level-2
+  (let [edges ["COM)B" "B)C" "C)D" "D)E" "E)F" "B)G" "G)H" "D)I" "E)J" "J)K" "K)L"]]
+    (is (= 0 (solve-2 edges "F" "J")))
+    (is (= 2 (solve-2 edges "K" "I"))))
+
+  (let [edges ["COM)B" "B)C" "C)D" "D)E" "E)F" "B)G" "G)H" "D)I" "E)J" "J)K" "K)L" "K)YOU" "I)SAN"]]
+    (is (= 4 (solve-2 edges "YOU" "SAN")))))
+
+(with-open [rdr (io/reader (io/resource "advent-of-code-2019/day06.in"))]
+  (solve-2 (line-seq rdr) "YOU" "SAN"))
+;; => 343
